8c377993c11ac014c45b0724c0472254a3c7132b
Subhankar Raj <subhankar.rj@gmail.com>
2021-02-14 22:51:46 +0530

    Implement add request

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,38 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// auth trigger (new user signup)
+exports.newUserSignUp = functions.auth.user().onCreate((user) => {
+  // for background triggers you must return a value/promise
+  return admin.firestore().collection("users").doc(user.uid).set({
+    email: user.email,
+    upvotedOn: [],
+  });
+});
+
+// auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete((user) => {
+  const doc = admin.firestore().collection("users").doc(user.uid);
+  return doc.delete();
+});
+
+// http callable function (adding a request)
+exports.addRequest = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+        "unauthenticated", 
+        "only authenticated users can add requests"
+    );
+  }
+  if (data.text.length > 30) {
+    throw new functions.https.HttpsError(
+        "invalid-argument", 
+        "request must be no more than 30 characters long"
+    );
+  }
+  return admin.firestore().collection("requests").add({
+    text: data.text,
+    upvotes: 0,
+  });
+});
